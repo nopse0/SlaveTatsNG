bac313785c9c6e23911850c0d963de94315e4d42
nopse0 <145904664+nopse0@users.noreply.github.com>
2024-12-20 15:56:49 +0100

    sync with local repo

--- deploy.bat
+++ deploy.bat
@@ -1,9 +1,10 @@
-set pluginsrc=C:\Build\SlaveTatsNG\build\Debug
+set pluginsrc=C:\Build\SlaveTatsNG\build\Release
+rem set pluginsrc=C:\Build\SlaveTatsNG\build\Debug
 set scriptssrc=C:\Build\SlaveTatsNG\papyrus\Scripts
-rem set plugindest="D:\Games\Skyrim-1.6.1170\overwrite"
-set plugindest="D:\Games\Skyrim-1.5.97\overwrite"
-rem set scriptsdest="D:\Games\Skyrim-1.6.1170\overwrite"
-set scriptsdest="D:\Games\Skyrim-1.5.97\overwrite"
+set plugindest="D:\Games\Skyrim-1.6.1170\overwrite"
+rem plugindest="D:\Games\Skyrim-1.5.97\overwrite"
+set scriptsdest="D:\Games\Skyrim-1.6.1170\overwrite"
+rem set scriptsdest="D:\Games\Skyrim-1.5.97\overwrite"
 copy %pluginsrc%\*.dll %plugindest%\SKSE\Plugins\
 copy %pluginsrc%\*.pdb %plugindest%\SKSE\Plugins\
 copy %scriptssrc%\*.pex %scriptsdest%\Scripts\
