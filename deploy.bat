bc7efa3f902b829668062ffea43c25a34b9c73a9
nopse0 <145904664+nopse0@users.noreply.github.com>
2025-05-05 16:09:47 +0200

    Another wrong derived path

--- deploy.bat
+++ deploy.bat
@@ -5,7 +5,7 @@ set configsrc=%root%\config
 set headersrc=%root%\include
 set scriptssrc=%root%\papyrus\Scripts
 set plugindest=%dest%\overwrite
-set scriptsdest="F:\Games\Skyrim-1.6.1170\overwrite"
+set scriptsdest=%dest%\overwrite
 copy %pluginsrc%\*.dll %plugindest%\SKSE\Plugins\
 copy %pluginsrc%\*.pdb %plugindest%\SKSE\Plugins\
 mkdir %plugindest%\SKSE\Plugins\SlaveTatsNG
